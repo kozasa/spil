93d3eabe3cb7e6fb1545fc2cceb5e7e3ba0cdf89
小笹敏輝 <kozasa@Kozas-PC.local>
2018-04-30 00:04:53 +0900

    fix

--- bot/index.php
+++ bot/index.php
@@ -4,6 +4,8 @@ require('../slim/src/settings_param.php');
 require('../slim/classes/utility/LineBotRecieve.php');
 require('../slim/classes/utility/LineBotMassage.php');
 require('../slim/classes/utility/LineBotPush.php');
+require('../slim/classes/mapper/Mapper.php');
+require('../slim/classes/mapper/PushMapper.php');
 
 $myclass = new MyBot;
 $myclass->main();

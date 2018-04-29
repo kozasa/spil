d80371538a96026c12fb1929fc52c01a512b4aaa
小笹敏輝 <kozasa@Kozas-PC.local>
2018-04-29 23:54:34 +0900

    fix

--- bot/index.php
+++ bot/index.php
@@ -144,6 +144,7 @@ class MyBot{
         // メッセージ確認
         require '/../slim/vendor/autoload.php';
         $settings = require '/../slim/src/settings.php';
+        error_log(print_r($settings, TRUE), 3, 'yamato_dbg_log.txt');
         $app = new \Slim\App($settings);
 
         $massage_text = $receive['events'][0]["message"]["text"];

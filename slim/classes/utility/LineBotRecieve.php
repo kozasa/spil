d87eef91870a28b454baa3b92c3df9aa62e29962
小笹敏輝 <kozasa@Kozas-PC.local>
2018-04-29 18:02:51 +0900

    fix

--- slim/classes/utility/LineBotRecieve.php
+++ slim/classes/utility/LineBotRecieve.php
@@ -15,10 +15,6 @@ class LineBotRecieve
      */
     public static function recieveMassage($massage_text){
 
-        error_log(print_r($massage_text, TRUE), 3, 'yamato_dbg_log.txt');
-
-        return $massage_text;
-        /*
         if(!strpos($massage_text,spilMan.'再通知')){
             //スピルくん再通知が含まれている場合
 
@@ -29,7 +25,7 @@ class LineBotRecieve
             Utility\LineBotPush::push($massage);
 
         }
-        */
+        
     }
 
 }

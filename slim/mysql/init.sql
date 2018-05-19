154b33e7f9b9c93291db8ffe5aae300e68dda849
小笹敏輝 <kozasa@Kozas-PC.local>
2018-05-19 13:33:07 +0900

    pipeline

---
+++ slim/mysql/init.sql
@@ -0,0 +1,113 @@
+CREATE DATABASE spil;
+USE spil;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- テーブルの構造 `admin_user`
+--
+
+CREATE TABLE `admin_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- テーブルの構造 `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `event_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `place` text NOT NULL,
+  `event_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `fee` int(11) NOT NULL,
+  `before_seven_days` tinyint(1) NOT NULL,
+  `before_one_day` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルの構造 `event_participants`
+--
+
+CREATE TABLE `event_participants` (
+  `id` int(10) NOT NULL,
+  `event_id` varchar(255) NOT NULL,
+  `member_id` varchar(255) NOT NULL,
+  `join_flag` tinyint(1) NOT NULL,
+  `new_flag` tinyint(1) NOT NULL,
+  `new_name` varchar(100) NOT NULL,
+  `new_gender` int(1) NOT NULL,
+  `new_age` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルの構造 `user_mst`
+--
+
+CREATE TABLE `user_mst` (
+  `id` int(10) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `display_name` varchar(50) NOT NULL,
+  `picture_url` varchar(255) NOT NULL,
+  `insert_date` date NOT NULL,
+  `delete_flg` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `event_participants`
+--
+ALTER TABLE `event_participants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_mst`
+--
+ALTER TABLE `user_mst`
+  ADD PRIMARY KEY (`id`,`user_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `event_participants`
+--
+ALTER TABLE `event_participants`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+
+--
+-- AUTO_INCREMENT for table `user_mst`
+--
+ALTER TABLE `user_mst`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
